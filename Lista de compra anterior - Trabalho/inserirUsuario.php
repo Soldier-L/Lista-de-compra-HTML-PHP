8b39081d0d95e11d267c6fc6bc108ce0f291609b
Jeferson Ferreira <jefender@gmail.com>
2020-10-20 21:33:40 -0300

    novo usuario

---
+++ Lista de compra anterior - Trabalho/inserirUsuario.php
@@ -0,0 +1,22 @@
+﻿<?php
+include "conexao.php";
+
+$nome = $_POST["txtNewName"];
+$email = $_POST["txtNewEmail"];
+$senha = $_POST["txtNewSenha"];
+
+$sqlComando = "INSERT INTO t_user_list_buy (usuario, email, senha) values('$nome','$email','$senha')";
+
+$stmt = $con->query($sqlComando);
+if ($con->query($sqlComando))
+{
+    echo "Login Correto<br>Acesso Liberado<br><br>";
+    echo "Usuario: $nome<br>";
+    echo "Senha: $senha"; 
+}
+else{
+    echo "Login Incorreto<br>Acesso Negado<br><br>";
+    echo "Usuario: $nome<br>";
+    echo "Senha: $senha"; 
+}
+?>
